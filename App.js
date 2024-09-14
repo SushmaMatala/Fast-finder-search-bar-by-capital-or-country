aaa21f79cdd65523b960504744d2e3cd2a48ee59
SushmaMatala <sushmamatala011@gmail.com>
2024-09-14 22:24:14 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import SearchBar from './components/SearchBar';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Fast Finder Search Bar</h1>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default App;
